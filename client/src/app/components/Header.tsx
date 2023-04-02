920c884e4a94cee72026a8c8df90a6bd8ba4dfb9
kristina-sparrow <118679841+kristina-sparrow@users.noreply.github.com>
2023-04-02 11:12:14 -0600

    Create header

--- client/src/app/components/Header.tsx
+++ client/src/app/components/Header.tsx
@@ -1,9 +1,22 @@
 import React from "react";
+import { FaBell } from "react-icons/fa";
 
 export default function Header() {
   return (
     <header className="header">
       <h1 className="header__title">Inventory Dashboard</h1>
+      <div className="header__tools">
+        <input type="text" id="search-bar" placeholder=" " />
+        <div className="divider"></div>
+        <div className="alerts">
+          <FaBell />
+        </div>
+        <div className="divider"></div>
+        <div className="header__user">
+          <div className="username">Kristina Sparrow</div>
+          <img className="avatar" src="../../assets/avatar.jpg" />
+        </div>
+      </div>
     </header>
   );
 }
